7697e75ee75de12acf3eeac30ea3ee67f8ca266c
rbbtsn0w <hamiltonsnow@gmail.com>
2026-07-08 10:26:45 +0800

    refactor: restructure main and runCLI methods for improved clarity

--- Sources/iDocsCLI/Main.swift
+++ Sources/iDocsCLI/Main.swift
@@ -5,12 +5,27 @@ import iDocsTelemetry
 @main
 struct Main {
     @available(macOS 10.15, macCatalyst 13, iOS 13, tvOS 13, watchOS 6, *)
-    nonisolated static func main() async {
+    static func main() async {
         let arguments = CommandLine.arguments
         let parsedArguments = Array(arguments.dropFirst())
         let version = CLIVersion.current()
         let environment = ProcessInfo.processInfo.environment
 
+        await runCLI(
+            arguments: arguments,
+            parsedArguments: parsedArguments,
+            version: version,
+            environment: environment
+        )
+    }
+
+    @available(macOS 10.15, macCatalyst 13, iOS 13, tvOS 13, watchOS 6, *)
+    private nonisolated static func runCLI(
+        arguments: [String],
+        parsedArguments: [String],
+        version: String,
+        environment: [String: String]
+    ) async {
         await iDocsTelemetry.withRootSpan(
             arguments: arguments,
             serviceVersion: version,
